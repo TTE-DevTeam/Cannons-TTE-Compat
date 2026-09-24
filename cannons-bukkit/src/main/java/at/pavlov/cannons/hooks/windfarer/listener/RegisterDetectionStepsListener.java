6d19aa94a22eabbb9e8f229eaf12a139ada34329
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2026-09-24 20:10:10 +0200

    add detectionSTep registration

--- cannons-bukkit/src/main/java/at/pavlov/cannons/hooks/windfarer/listener/RegisterDetectionStepsListener.java
+++ cannons-bukkit/src/main/java/at/pavlov/cannons/hooks/windfarer/listener/RegisterDetectionStepsListener.java
@@ -1,9 +1,16 @@
 package at.pavlov.cannons.hooks.windfarer.listener;
 
+import at.pavlov.cannons.hooks.windfarer.task.CannonDetectionTask;
+import net.countercraft.movecraft.craft.Craft;
+import net.countercraft.movecraft.craft.NullCraft;
+import net.countercraft.movecraft.craft.SinkingCraft;
 import net.countercraft.movecraft.events.CraftGatherAdditionalDetectionStepsEvent;
+import net.countercraft.movecraft.processing.effects.Effect;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+
+import java.util.function.Supplier;
 
 public class RegisterDetectionStepsListener implements Listener {
 
@@ -12,6 +19,16 @@ public class RegisterDetectionStepsListener implements Listener {
     public void onDetectAddSteps(final CraftGatherAdditionalDetectionStepsEvent event) {
         // Cannon detection
         // Can return null, if that step is not to be run for that craft!
+        event.addStep(RegisterDetectionStepsListener::cannonDetectionStepBuilder);
     }
+
+    private static Supplier<Effect> cannonDetectionStepBuilder(Supplier<Effect> effectSupplier, Craft craft) {
+        // Invalid crafts
+        if (craft instanceof SinkingCraft || craft instanceof NullCraft) {
+            return null;
+        }
+        return new CannonDetectionTask(craft);
+    }
+
 
 }
