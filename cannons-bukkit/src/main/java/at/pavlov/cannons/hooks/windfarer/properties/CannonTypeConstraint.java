ffd394541b5933ea3cc5ec84b3e14dfc9181ba2c
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2026-09-24 20:43:33 +0200

    add CannonTypeConstriant helper object

---
+++ cannons-bukkit/src/main/java/at/pavlov/cannons/hooks/windfarer/properties/CannonTypeConstraint.java
@@ -0,0 +1,123 @@
+package at.pavlov.cannons.hooks.windfarer.properties;
+
+import net.countercraft.movecraft.craft.Craft;
+import net.countercraft.movecraft.craft.type.TypeData;
+import net.countercraft.movecraft.util.Pair;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+import org.bukkit.configuration.serialization.SerializableAs;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@SerializableAs("CannonsRevamped_CannonTypeConstraint")
+public record CannonTypeConstraint(
+        List<String> cannonTypes,
+        boolean numericMin,
+        Optional<Double> min,
+        boolean numericMax,
+        Optional<Double> max
+) implements ConfigurationSerializable {
+
+    /**
+     *
+     * @return Empty if no error, otherwise return the error
+     */
+    public Optional<String> detect(int count, int size) {
+        final double blockPercent = 100D * count / size;
+        if (min.isPresent()) {
+            final double minD = min.get();
+            if (numericMax) {
+                if (count < minD)
+                    return Optional.of(String.format("You have too few %s cannon types : %d < %d", allNames(), count, (int) minD));
+            } else {
+                if (blockPercent < minD)
+                    return Optional.of(String.format("You have too few %s cannon types : %.2f%% < %.2f%%", allNames(), blockPercent, minD));
+            }
+        }
+
+        if (max.isPresent()) {
+            final double maxD = max.get();
+            if (numericMax) {
+                if (count > maxD)
+                    return Optional.of(String.format("You have too many %s cannon types : %d > %d", allNames(), count, (int) maxD));
+            } else {
+                if (blockPercent > maxD)
+                    return Optional.of(String.format("You have too many %s cannon types : %.2f%% > %.2f%%", allNames(), blockPercent, maxD));
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    public String allNames() {
+        return String.join(", ", cannonTypes);
+    }
+
+    public Optional<String> check(Craft craft, Map<String, Integer> cannonCountMap) {
+        int count = 0;
+        for (String name : cannonTypes) {
+            count += cannonCountMap.getOrDefault(name, 0);
+        }
+
+        return detect(count, craft.getOrigBlockCount());
+    }
+
+    @Override
+    public @NotNull Map<String, Object> serialize() {
+        return Map.of(
+                "cannonTypes", this.allNames(),
+                "min", this.numericMin ? TypeData.NUMERIC_PREFIX : "" + this.min,
+                "max", this.numericMax ? TypeData.NUMERIC_PREFIX : "" + this.max
+        );
+    }
+
+    static Pair<Boolean, ? extends Number> parseLimit(@NotNull Object input) {
+        if (input == null) {
+            return null;
+        }
+        if (input instanceof String) {
+            String str = (String) input;
+            if (str.contains(TypeData.NUMERIC_PREFIX)) {
+                String[] parts = str.split(TypeData.NUMERIC_PREFIX);
+                int val = Integer.parseInt(parts[1]);
+                return new Pair<>(true, val);
+            }
+            else
+                return new Pair<>(false, Double.valueOf(str));
+        }
+        else if (input instanceof Integer) {
+            return new Pair<>(false, (Integer) input);
+        }
+        else
+            return new Pair<>(false, (double) input);
+    }
+
+    public static @NotNull CannonTypeConstraint deserialize(@NotNull Map<String, Object> yamlData) {
+        List<String> typeList = new ArrayList<>();
+        Object listRaw = yamlData.getOrDefault("cannonTypes", "");
+        if (listRaw instanceof String rawString) {
+            for (String subString : rawString.split(",")) {
+                typeList.add(subString);
+            }
+        } else if (listRaw instanceof List rawList) {
+            for (Object object : rawList) {
+                if (object instanceof String stringTmp) {
+                    typeList.add(stringTmp);
+                }
+            }
+        }
+        Pair<Boolean, ? extends Number> min = parseLimit(yamlData.getOrDefault("min", null));
+        Pair<Boolean, ? extends Number> max = parseLimit(yamlData.getOrDefault("max", null));
+
+        return new CannonTypeConstraint(
+            typeList,
+            min != null ? min.getLeft() : false,
+            Optional.ofNullable(min != null ? min.getRight().doubleValue() : null),
+            max != null ? max.getLeft() : false,
+            Optional.ofNullable(max != null ? max.getRight().doubleValue() : null)
+        );
+    }
+}
